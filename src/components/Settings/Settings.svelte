3e8b6d24e5af885ab4e38f3cd8f5db1f4fd52361
Maxence Robinet <maxencerobinet@gmail.com>
2024-01-16 12:06:12 +0100

    removing pixel ratio statement qui ne sert à rient

--- src/components/Settings/Settings.svelte
+++ src/components/Settings/Settings.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import { appStore, filterSettingStore, konvaStore, userStore} from "stores";
+  import { appStore, filterSettingStore, konvaStore, userStore } from "stores";
 
   import Konva from "konva";
   import { initFiltersValue } from "lib/default";
   import { handleFilterchange } from "lib/filters";
-
-  let pixelRatio: number;
 
   let {
     minBlurValue,
@@ -23,10 +21,6 @@
     minNoiseValue,
     maxNoiseValue,
   } = initFiltersValue;
-
-  $: {
-    pixelRatio = $appStore.pixelRatio;
-  }
 </script>
 
 <div class="settings">
