9d183c95ea369db10dcc71e0d8e89ca7d47e9e33
Maxence Robinet <maxencerobinet@gmail.com>
2024-01-16 02:37:44 +0100

    dodo

--- src/components/ApplicationHelp/ApplicationHelp.svelte
+++ src/components/ApplicationHelp/ApplicationHelp.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+  import { appStore } from "stores";
 </script>
 
-<p>
-  When hovering hover the background, use mouse scroll to zoom in and out.
-</p>
-<p>
-  When you're done, click on the download icon to download the image.
-</p>
+{#if $appStore.automaticMode}
+  <p>You're in <span class="automatic">automatic</span> mode. Enter manual mode to access filer settings.</p>
+{/if}
+<p>When hovering hover the background, use mouse scroll to zoom in and out.</p>
+<p>When you're done, click on the download icon to download the image.</p>
 
 <style lang="scss">
   p {
     font-size: 0.9rem;
     line-height: 0.8rem;
+  }
+
+  .automatic {
+    color: var(--primary)
   }
 </style>
